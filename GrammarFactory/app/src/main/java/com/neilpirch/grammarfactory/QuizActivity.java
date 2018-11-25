07d92c52b9b6be4ed272a0249d1d828ac2c5bbf9
neilpirch <neil.pirch@gmail.com>
2018-11-25 12:18:48 -0600

    Countdown Timer Works

--- GrammarFactory/app/src/main/java/com/neilpirch/grammarfactory/QuizActivity.java
+++ GrammarFactory/app/src/main/java/com/neilpirch/grammarfactory/QuizActivity.java
@@ -1,7 +1,9 @@
 package com.neilpirch.grammarfactory;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -14,8 +16,12 @@ import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
+    public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
+
     private TextView textViewQuestion;
     private TextView textViewScore;
     private TextView textViewQuestionCount;
@@ -25,6 +31,10 @@ public class QuizActivity extends AppCompatActivity {
     private Button buttonConfirmNext;
 
     private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
 
     private List<QuizQuestion> quizQuestionList;
 
@@ -34,6 +44,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private int score;
     private boolean answered;
+
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +64,7 @@ public class QuizActivity extends AppCompatActivity {
         buttonConfirmNext = findViewById(R.id.button_confirm_pos);
 
         textColorDefaultRb = rb1.getTextColors();
+        textColorDefaultCd = textViewCountdown.getTextColors();
 
         QuizDBHelper dbHelper = new QuizDBHelper(this);
         quizQuestionList = dbHelper.getAllQuestions();
@@ -99,13 +112,50 @@ public class QuizActivity extends AppCompatActivity {
             textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
             answered = false;
             buttonConfirmNext.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
         } else {
             finishQuiz();
         }
     }
 
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 100) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMillis = l;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeFormatted = String.format(Locale.getDefault(), "$02d:%02d", minutes, seconds);
+
+        textViewCountdown.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000) {
+            textViewCountdown.setTextColor(Color.RED);
+        } else {
+            textViewCountdown.setTextColor(textColorDefaultCd);
+        }
+    }
+
     private void checkAnswer() {
         answered = true;
+
+        countDownTimer.cancel();
 
         RadioButton rbSelected = findViewById(radioGroup.getCheckedRadioButtonId());
         int answerNum = radioGroup.indexOfChild(rbSelected) + 1;
@@ -159,7 +209,29 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void finishQuiz() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
         finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finishQuiz();
+        } else {
+            Toast.makeText(this, "Press back again to finish quiz", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
 
--- GrammarFactory/app/src/main/java/com/neilpirch/grammarfactory/StartingScreenActivity.java
+++ GrammarFactory/app/src/main/java/com/neilpirch/grammarfactory/StartingScreenActivity.java
@@ -1,17 +1,27 @@
 package com.neilpirch.grammarfactory;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingScreenActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighScore";
+
+    private TextView textViewHighScore;
+    private int highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        textViewHighScore = findViewById(R.id.text_view_highscore_pos);
 
         Button buttonStartQuizPos = findViewById(R.id.button_start_quiz);
         buttonStartQuizPos.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +34,35 @@ public class StartingScreenActivity extends AppCompatActivity {
 
     private void startQuizPos() {
         Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highScore) {
+                    updateHighScore(score);
+                }
+            }
+        }
+    }
+
+    private void loadHighScore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highScore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighScore.setText("High Score: " + highScore);
+    }
+
+    private void updateHighScore(int highScoreNew) {
+        highScore = highScoreNew;
+        textViewHighScore.setText("High Score: " + highScore);
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highScore);
+        editor.apply();
     }
 }
